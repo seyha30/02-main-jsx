c429fc9a77791b9f8b33f9e9d4dbaaebc8d2c31d
seyha30 <sinseyha1x@gmail.com>
2023-03-13 20:39:37 +0700

    com

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const user = { firstName: "sin", lastName: "seyha" };
+
+  function formatName(user) {
+    return user.firstName + "" + user.lastName;
+  }
+  const myName = <h4>SSEYHA {formatName(user)}</h4>;
+
+  function getGreeting(user) {
+    if (user) return <h1>Hello, {formatName(user)}</h1>;
+    return <h1>Hello , Strnager .</h1>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <h1>Hello, World {myName}</h1>
+    //  <h1> HI {formatName(user)}</h1>
+    <h2> {getGreeting(user)}</h2>
   );
 }
 
